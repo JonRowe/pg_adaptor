7a3a7b2b799d062b677c745609d9e0707085d18d
Jon Rowe <hello@jonrowe.co.uk>
2016-10-21 15:22:16 +1100

    inserting

--- lib/pg_adaptor.rb
+++ lib/pg_adaptor.rb
@@ -1,7 +1,26 @@
+require 'sequel'
 require "pg_adaptor/version"
 
 class PGAdaptor
   class << self
     attr_accessor :db
   end
+
+  def initialize name, klass
+    @table = self.class.db[name]
+    @klass = klass
+  end
+
+  def insert model
+    @table.insert process(model)
+  end
+
+private
+
+  def process(model)
+    fields = {}
+    model.each_pair { |field,value| fields[field] = value unless field == :id }
+    fields
+  end
+
 end
--- spec/pg_adaptor_spec.rb
+++ spec/pg_adaptor_spec.rb
@@ -9,4 +9,39 @@ RSpec.describe 'adapting structs into pg' do
     end
   end
 
+  describe 'using the adaptor' do
+    let(:klass)      { Struct.new :name, :other, :members, :id }
+    let(:adaptor)    { PGAdaptor.new :test_table, klass }
+    let(:table)      { db[:test_table] }
+
+    describe 'with a new model' do
+      let(:model) { klass.new 'Test Model','Some Data','Some Members','fake key'  }
+      let(:data)  { table.order(:id).last }
+
+      shared_examples_for 'creates a record' do
+        it 'changes the number of items in the table' do
+          expect { perform }.to change { table.count }.by(1)
+        end
+        it 'generates an id, ignoring any set key' do
+          perform
+          expect(data[:id]).to be_a Integer
+        end
+      end
+
+      shared_examples_for 'new model' do
+        it_should_behave_like 'creates a record'
+        it 'sets my fields and values' do
+          perform
+          expect(data[:name]).to  eq 'Test Model'
+          expect(data[:other]).to eq 'Some Data'
+          expect(data[:members]).to eq 'Some Members'
+        end
+      end
+
+      context 'inserting' do
+        let(:perform) { adaptor.insert model }
+        it_should_behave_like 'new model'
+      end
+    end
+  end
 end
